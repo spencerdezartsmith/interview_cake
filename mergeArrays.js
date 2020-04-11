c50524dc85a3ab8fd103b994c48af64889ad90ff
spencer.dezart-smith <spencer.dezart.smith@gmail.com>
2020-04-11 20:39:21 +1000

    [master] Final solution

--- mergeArrays.js
+++ mergeArrays.js
@@ -1,38 +1,24 @@
-// In order to win the prize for most cookies sold, my friend Alice and I are going to merge our Girl Scout Cookies orders and enter as one unit.
-// Each order is represented by an "order id" (an integer).
-// We have our lists of orders sorted numerically already, in arrays. Write a function to merge our arrays of orders into one sorted array.
-
-// For example:
-const myArray = [2, 3, 4, 5, 6, 10, 11, 15];
-const alicesArray = [7, 8, 12, 14, 19];
-
-
-function mergeArrays(arrayOne, arrayTwo) {
+function mergeArrays(myArray, aliceArray) {
   const mergedArray = [];
-  const combinedLength = arrayOne.length + arrayTwo.length;
-  
-  let firstArrayStartIdx = 0;
-  let secondArrayStartIdx = 0;
-  
-  for (let i = 0; i < combinedLength; i++) {
-    if (firstArrayStartIdx >= arrayOne.length) {
-      return mergedArray.push(...arrayTwo.splice(secondArrayStartIdx));
-    } else if (secondArrayStartIdx >= arrayTwo.length) {
-      return mergedArray.push(arrayOne.splice(firstArrayStartIdx);
+
+  let currentIdxMergedMine = 0;
+  let currentIdxMergedAlice = 0;
+  let currentIdxMerged = 0;
+
+  while (currentIdxMerged < (myArray.length + aliceArray.length)) {
+    let isEndOfArrayMine = currentIdxMergedMine >= myArray.length;
+    let isEndOfArrayAlice = currentIdxMergedAlice >= aliceArray.length;
+
+    if (!isEndOfArrayMine && (isEndOfArrayAlice || myArray[currentIdxMergedMine] < aliceArray[currentIdxMergedAlice])) {
+      mergedArray[currentIdxMerged] = myArray[currentIdxMergedMine];
+      currentIdxMergedMine++
+    } else {
+      mergedArray[currentIdxMerged] = aliceArray[currentIdxMergedAlice];
+      currentIdxMergedAlice++
     }
 
-    if (arrayOne[firstArrayStartIdx] < arrayTwo[secondArrayStartIdx]) {
-      mergedArray[i] = arrayOne[firstArrayStartIdx];
-      firstArrayStartIdx++
-    } else {
-      mergedArray[i] = arrayTwo[secondArrayStartIdx];
-      secondArrayStartIdx++;
-    }
+    currentIdxMerged++
   }
 
   return mergedArray;
 }
-
-
-console.log(mergeArrays(myArray, alicesArray));
-// logs [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19]
